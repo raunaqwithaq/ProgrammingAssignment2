02c1ff2da96f4230be871158a7c0a93b814a01f2
raunaqwithaq <62586734+raunaqwithaq@users.noreply.github.com>
2020-04-15 12:13:37 +0530

    Update and rename cachematrix.R to matrix.R

--- matrix.R
+++ matrix.R
@@ -1,8 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 a<-NULL
 	set<-function(y)
@@ -16,8 +12,11 @@ a<-NULL
 	list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
